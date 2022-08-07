ab030995f491800bc21a2b1a96bcb87c2b077bee
fidan2004 <110298605+fidan2004@users.noreply.github.com>
2022-08-07 06:46:27 +0400

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pls enter text to count spaces");
+            string text = Console.ReadLine();
+            Console.WriteLine("the num of spaces" + sp_c(text));
+        }
+        public static int sp_c(string text) 
+        {
+            
+            int count = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    count++;
+                }
+            }
+        }
+    }
+}
